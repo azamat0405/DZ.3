52467387c7ee82d362235c7d4677a0ce2f4d8736
azamat0405 <azamat040@bk.ru>
2020-02-26 21:57:31 +0300

    на проверку

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double[] num = {9.1, -9.2, 9.3, 9.4, 9.5, -9.6, 9.7, -9.8, 9.9, 10.1, 10.2, 11.1, 12.3, 13.4, -14.8};
+        for (double tempNum : num) {
+            System.out.println(tempNum);
+
+        }
+        for (double tempNum : num) {
+            if (d ) {
+                 += tempNum;// тут ошибки
+                ++;          // тут ошибки
+            }
+            if (tempNum < 0) {
+                b = true;
+            }
+
+        }
+        result = //что мне тут надо вписыать /  //что мне тут надо вписыать;// тут ошибки
+        System.out.println(result);// тут ошибки
+    }
+
+}
+
